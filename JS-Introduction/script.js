9722a5da2d08b0904a084064da3131aa5b75e987
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-06 18:13:21 +0300

    JS strings, numbers, variables,

---
+++ JS-Introduction/script.js
@@ -0,0 +1,61 @@
+console.log('Hello world================================')
+console.log("Hello world");
+
+console.log('Strings================================')
+console.log('One');
+console.log("Two");
+console.log(`Three`);
+
+console.log('Numbers================================')
+console.log(8);
+console.log(80.5);
+
+console.log('Variables================================')
+let language = 'JavaScript'
+console.log(language);
+
+let number = 5 
+console.log(number);
+
+let name = 'Kestutis'
+console.log(name);
+
+name = 'Jonas'
+console.log(name);
+
+let surname = 'Eirosius'
+console.log(surname);
+
+name = surname
+console.log(name);
+
+const passportNumber = 45896
+console.log(passportNumber);
+
+// passportNumber = 45325
+// console.log(passportNumber);
+// value cannot be changed
+
+console.log('Undefined================================')
+let animal 
+console.log(animal);
+
+// const house;
+// console.log(house);
+// error
+
+let fruit = 'apple'
+console.log(fruit);
+
+fruit = undefined
+console.log(fruit);
+
+console.log('Variables and string in single line================================')
+
+let city = 'Vilnius'
+console.log('City:' + city);
+console.log(`City:${city}`);
+
+let kfcLocations = 10;
+console.log('City: ' + city + ',' + ' KFC locations: ' + kfcLocations);
+console.log(`City: ${city}, KFC locations: ${kfcLocations}`);
