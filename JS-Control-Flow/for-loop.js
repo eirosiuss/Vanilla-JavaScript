08d7c0a80fc8e5f644f48f4473a26fb6f3c3e06c
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-10 20:17:48 +0300

    JS if

--- JS-Control-Flow/for-loop.js
+++ JS-Control-Flow/for-loop.js
@@ -1,1 +1,44 @@
 console.log('For loop================================');
+for (let index = 1; index <= 5; index++) {
+    console.log('For loop');
+    console.log(index);
+}
+
+console.log('Multiplication Table---------------');
+let number = 5;
+
+for (let index = 1; index <= 10; index++) {
+    let result = number * index;
+    console.log(number+ 'x'+ index+ '='+ result);
+}
+
+console.log('For in loop================================');
+let student = {
+    name: 'Monica',
+    grade: 7,
+    age: 12
+}
+
+for (let data in student) {
+    console.log(`${data} => ${student[data]}`);
+}
+
+console.log('For of loop================================');
+let students = ['John', 'Sara', 'Jack']
+for (let data of students) {
+    console.log(data);
+}
+
+let language = 'JavaScript';
+
+for (let character of language) {
+    console.log(character);
+}
+
+console.log('---------------------');
+for (let index = 1; index <= 100; index++) {
+    if (index % 2 == 0) {
+        console.log(index);
+        
+    }
+}
--- JS-Control-Flow/if-else.js
+++ JS-Control-Flow/if-else.js
@@ -185,3 +185,71 @@ switch (month) {
         console.log('Invalid');
         break;
 }
+
+console.log('1================================');
+// Susikurkite tris kintamuosius skaičiams saugoti. Parašykite šias atskiras if sąlygas:
+// Ar pirmas ir antras skaičiai yra lygūs?
+// Ar antras ir trečias skaičiai yra lygūs?
+// Ar pirmas skaičius yra didesnis už antrąjį?
+// Ar antras skaičius yra didesnis už dvigubą trečiojo skaičiaus reikšmę (trečias skaičius padaugintas iš 2)?
+// Ar pirmas skaičius yra lyginis (ar dalinasi iš 2)?
+// Ar antras skaičius yra nelyginis (ar nesidalina iš 2)?
+// Ar trečias skaičius yra teigiamas (didesnis už 0)?
+// Ar pirmas skaičius yra neigiamas (mažesnis už 0)?
+// Ar antras skaičius dalinasi iš 4 be liekanos?
+// Ar trečias skaičius dalinasi iš 8 be liekanos?
+
+let noOne = 2;
+let noTwo = 3;
+let noThree = 4;
+
+console.log(noOne == noTwo ? 'lygus' : 'nelygus');
+console.log(noTwo == noThree ? 'lygus' : 'nelygus');
+console.log(noOne > noTwo ? 'didesnis' : 'mažesnis');
+console.log(noTwo > noThree * 2 ? 'didesnis' : 'mažesnis');
+console.log(noOne % 2 === 0 ? 'lyginis' : 'nelyginis');
+console.log(noTwo % 2 !== 0 ? 'nelyginis' : 'lyginis');
+console.log(noThree > 0 ? 'teigiamas' : 'neigiamas');
+console.log(noOne < 0 ? 'teigiamas' : 'neigiamas');
+console.log(noTwo % 4 === 0 ? 'dalinasi' : 'nesidalina');
+console.log(noThree % 8 === 0 ? 'dalinasi' : 'nesidalina');
+
+console.log('2================================');
+// Susikurkite kintamąjį vartotojo amžiui saugoti. Patikrinkite ar amžius yra didesnis arba lygus 18-ai, jei taip - išveskite “jūs galite balsuoti”.
+let adult = 17;
+console.log(adult >= 18 ? 'balsuojate' : 'nebalsuojate');
+
+
+console.log('3================================');
+// Susikurkite kelis kintamuosius saugoti pažymiams. Raskite šių pažymių vidurkį. Patikrinkite ar vidurkis teigiamas (daugiau arba lygu 4-iems), jei taip - išveskite “vidurkis teigiamas”.
+let mark1 = 3;
+let mark2 = 4;
+let mark3 = 4;
+
+console.log(((mark1 + mark2 + mark3) / 3) >= 4 ? 'teigiamas' : 'neigiamas');
+
+console.log('4================================');
+// Susikurkite skaičiaus kintamąjį. Atlikite šiuos patikrinimus ir veiksmus:
+// Jei skaičius dalinasi iš 5, tuomet išveskite šio skaičiaus daugybos lentelę nuo 1 iki 5.
+// Jei skaičius lyginis, tuomet išveskite šį skaičių, jo kvadratą ir jį padalintą iš 2.
+// Jei skaičius nesidalina iš 7, tuomet susikurkite antrąjį kintamąjį, išveskite šių dviejų skaičių sumą, skirtumą, sandaugą, dalmenį.
+
+let noFour = 5;
+
+if (noFour % 5 === 0) {
+    console.log(
+    '1 x ' + noFour + ' =', 1 * noFour, '\n' +
+    '2 x ' + noFour + ' =', 2 * noFour, '\n' +
+    '4 x ' + noFour + ' =', 3 * noFour, '\n' +
+    '5 x ' + noFour + ' =', 5 * noFour, '\n'
+)
+}
+
+if (noFour % 2 === 0) {
+    console.log(noFour, noFour * noFour, noFour / 2)
+}
+
+if (noFour % 7 !== 0) {
+    let noFive = 2;
+    console.log(noFour + noFive, noFour - noFive, noFour * noFive, noFour / noFive); 
+}
