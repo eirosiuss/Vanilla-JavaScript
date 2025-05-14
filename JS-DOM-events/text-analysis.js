2fde94d929ae8bedb95f7cd76d6f75e64dd97ba8
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-14 16:37:16 +0300

    JS text-analysis

--- JS-DOM-events/text-analysis.js
+++ JS-DOM-events/text-analysis.js
@@ -1,17 +1,37 @@
 let analyzeButton = document.querySelector('#analyze-button')
 
+let clearButton = document.querySelector('#clear-button')
+clearButton.style.display = 'none'
+
 let text = document.querySelector('#analyze-text')
+
+let error = document.querySelector('#error')
 
 let words = document.querySelector('#words')
 
 let characters = document.querySelector('#characters')
 
 analyzeButton.addEventListener('click', () => {
-    words.style.display = 'block'
-    characters.style.display = 'block'
 
-    words.firstElementChild.textContent = text.value.trim().split(/\s+/).filter(w => w).length
+    if (text.value === '') {
+        error.style.display = 'block'
+        error.style.color = 'red'
+    } else if (text.value !== '') {
+        words.style.display = 'block'
+        characters.style.display = 'block'
+        error.style.display = 'none'
 
-    characters.firstElementChild.textContent = text.value.length
+        words.firstElementChild.textContent = text.value.replace(/[^a-zA-Z\s]/g, '').trim().split(/\s+/).filter(w => w).length
 
+        characters.firstElementChild.textContent = text.value.length
+
+        clearButton.style.display = 'block'
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    text.value = ''
+    clearButton.style.display = 'none'
+    words.style.display = 'none'
+    characters.style.display = 'none'
 })
