a4706319a174e4a6c48b5f23edd6732226932ba7
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-15 17:52:30 +0300

    Add error handling and reset feature

--- JS-DOM-events/bakery-shop.js
+++ JS-DOM-events/bakery-shop.js
@@ -8,15 +8,65 @@ calculateButton.addEventListener('click', () => {
     let bakeryTotal = employeesCount * employeeMakes
 
     let bakeryTarget = bakeryTotal >= dayOrders
-        
-    // let logInfo = {
-    //     bakeryTotal,
-    //     bakeryTarget
-    // }
-    // console.log(logInfo);
 
     let results = document.getElementById('results')
-    // console.log(results);
-    
-    results.innerHTML = 'asd'
+
+    if ((employeesCount || employeeMakes || dayOrders) < 0) {
+    } else {
+        results.innerHTML = `<p>The bakery bakes <strong>${bakeryTotal}</strong> loaves of bread per day</p>`
+        results.innerHTML += `<p>Today's orders: <strong>${dayOrders}</strong></p>`
+        results.innerHTML += `<p>${bakeryTarget ? "We managed to fulfill today's orders." : "We didn't manage to fulfill today's orders."}</p>`
+    }
+})
+
+document.getElementById('employees-count').addEventListener('input', (evt) => {
+    let inputValue = evt.target.valueAsNumber
+
+    if (inputValue < 0) {
+        evt.target.classList.add('error')
+        evt.target.nextElementSibling.classList.add('show')
+    } else {
+        evt.target.classList.remove('error')
+        evt.target.nextElementSibling.classList.remove('show')
+    }
+})
+
+document.getElementById('employee-makes').addEventListener('input', (evt) => {
+    let inputValue = evt.target.valueAsNumber
+
+    if (inputValue < 0) {
+        evt.target.classList.add('error')
+        evt.target.nextElementSibling.classList.add('show')
+    } else {
+        evt.target.classList.remove('error')
+        evt.target.nextElementSibling.classList.remove('show')
+    }
+})
+
+document.getElementById('orders').addEventListener('input', (evt) => {
+    let inputValue = evt.target.valueAsNumber
+
+    if (inputValue < 0) {
+        evt.target.classList.add('error')
+        evt.target.nextElementSibling.classList.add('show')
+    } else {
+        evt.target.classList.remove('error')
+        evt.target.nextElementSibling.classList.remove('show')
+    }
+})
+
+
+
+document.getElementById('reset').addEventListener('click', () => {
+    document.getElementById('employees-count').value = '0'
+    document.getElementById('employee-makes').value = '0'
+    document.getElementById('orders').value = '0'
+    document.getElementById('results').innerHTML = '<p>Nothing at the moment.</p>'
+    document.getElementById('employees-count').nextElementSibling.classList.remove('show')
+    document.getElementById('employees-count').classList.remove('error')
+    document.getElementById('employee-makes').nextElementSibling.classList.remove('show')
+    document.getElementById('employee-makes').classList.remove('error')
+    document.getElementById('orders').nextElementSibling.classList.remove('show')
+    document.getElementById('orders').classList.remove('error')
+
 })
