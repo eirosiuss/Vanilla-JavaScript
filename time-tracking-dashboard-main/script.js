3c2bf9feed71a866d6c5cae9d362ca7a133d8874
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-27 11:59:53 +0300

    Add functionality to populate daily, weekly and monthly data from JSON and display in the DOM

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -1,13 +1,30 @@
 async function populate() {
     const requestURL =
-        "/Vanilla-JavaScript/time-tracking-dashboard-main/data.json"
+        "./data.json"
     const request = new Request(requestURL)
 
     const response = await fetch(request)
     const data = await response.json()
 
     populateHeader(data)
+    populateDaily(data)
+
+    dailyBtn.addEventListener('click', () => {
+        populateDaily(data)
+    })
+
+    weeklyBtn.addEventListener('click', () => {
+        populateWeekly(data)
+    })
+
+    monthlyBtn.addEventListener('click', () => {
+        populateMonthly(data)
+    })
 }
+
+const dailyBtn = document.querySelector('#daily')
+const weeklyBtn = document.querySelector('#weekly')
+const monthlyBtn = document.querySelector('#monthly')
 
 function populateHeader(obj) {
     const cardHeader = document.querySelectorAll('.header')
@@ -17,6 +34,39 @@ function populateHeader(obj) {
         const cardH2 = document.createElement('h2')
         cardH2.textContent = element.title
         cardHeader[index].appendChild(cardH2)
+    })
+}
+
+const currentHours = document.querySelectorAll('#current')
+const previousHours = document.querySelectorAll('#previous')
+
+function populateDaily(obj) {
+    obj.forEach((element, index) => {
+        const currentNumber = element.timeframes.daily.current
+        currentHours[index].textContent = currentNumber
+
+        const previousNumber = element.timeframes.daily.previous
+        previousHours[index].textContent = previousNumber
+    })
+}
+
+function populateWeekly(obj) {
+    obj.forEach((element, index) => {
+        const currentNumber = element.timeframes.weekly.current
+        currentHours[index].textContent = currentNumber
+
+        const previousNumber = element.timeframes.weekly.previous
+        previousHours[index].textContent = previousNumber
+    })
+}
+
+function populateMonthly(obj) {
+    obj.forEach((element, index) => {
+        const currentNumber = element.timeframes.monthly.current
+        currentHours[index].textContent = currentNumber
+
+        const previousNumber = element.timeframes.monthly.previous
+        previousHours[index].textContent = previousNumber
     })
 }
 
